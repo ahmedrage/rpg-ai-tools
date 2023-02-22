de0a763cbb1651d80962609e814e8f00e2900193
Ahmed Azhar <ahmed@hardsoftware.com>
2023-02-23 05:30:57 +1030

    setup

--- rpg_ai_tools/asgi.py
+++ rpg_ai_tools/asgi.py
@@ -4,8 +4,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from rpg_app.consumers import MyConsumer
 from django.urls import path
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rpg_ai_tools.settings')
+django.setup()
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
